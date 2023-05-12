48537da20a1d4c860a3457aee613671aecb33f96
junior-macena <brjunior834@gmail.com>
2023-05-12 19:08:57 -0400

    Modificacoes front-end

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="http://localhost/rodalivre2023/css/main.css">
     <link rel="stylesheet" type="text/css" href="http://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <title>Roda Livre</title>
 </head>
 
@@ -15,11 +16,11 @@
         <main>
             <div class="header-1">
                 <div class="logo">
-                    <img src="./img/logo-roda.png" />
+                    <img src="./img/logo.png" />
                 </div>
                 <div class="redes-sociais">
                     <ul>
-                        <li><a href=""><img src="./img/instagram.png"></a></li>
+                        <li><a href="https://www.instagram.com/juuniorcs/" target="_blank"><img src="./img/instagram.png"></a></li>
                     </ul>
                 </div>
             </div>
@@ -51,21 +52,100 @@
         <div class="content texto-destaque">
             <h1>OS <strong>MELHORES CARROS</strong> PARA VOCÊ ESTÃO AQUI</h1>
             <p>Confira a nossa ampla seleção de veículos para alugar, desde carros de luxo até econômicos, e desfrute da
-                liberdade de explorar novos lugares no seu próprio ritmo. Com a nossa frota de veículos bem
-                cuidados e a nossa equipe amigável e experiente, estamos aqui para ajudá-lo a tornar a sua próxima
-                aventura ainda mais memorável.</p>
+                liberdade de explorar novos lugares no seu próprio ritmo.</p>
 
-            <div class="col-3">
-
+            <div class="col-3 bloco-texto" style="margin-top: 6em;">
+                <img src="./img/content-1.png" />
+                <h3>Descubra</h3>
+                <p>Explore nosso site e encontre o veículo perfeito para sua próxima aventura.</p>
             </div>
-            <div class="col-3">
-
+            <div class="col-3 bloco-texto" style="margin-top: 8em;">
+                <img src="./img/content-2.png" />
+                <h3>Encontre o carro perfeito </h3>
+                <p>Com a nossa frota de veículos bem cuidados, estamos aqui para ajudá-lo a tornar a sua próxima
+                    aventura ainda mais memorável.</p>
             </div>
-            <div class="col-3">
-
+            <div class="col-3 bloco-texto" style="margin-top: 4em">
+                <img src="./img/content-3.png" />
+                <h3>Atendimento</h3>
+                <p>Com a nossa equipe amigável e experiente, estamos aqui para ajudar você a resolver qualquer problema,
+                    24 horas por dia, 7 dias por semana.</p>
             </div>
         </div>
     </div>
+    <div class="col-100 bloco-imagens-texto">
+        <div class="content" style="margin-left: 24%; float: left;">
+            <div class="col-3 bloco-texto bloco-imagem">
+                <img src="./img/carros/chevrolet_spin-2020-2021.png">
+                <p><b>Chevrolet SPIN</b></p>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting of the
+                    industry. Lorem
+                    Ipsum has been
+                    the and scrambled it.
+                    atype specimen
+                </p>
+            </div>
+            <div class="col-3 bloco-texto bloco-imagem">
+                <img src="./img/carros/fiat-fiorino.png">
+                <p><b>Fiat FIORINO</b></p>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting of the
+                    industry. Lorem
+                    Ipsum has been
+                    the and scrambled it.
+                    atype specimen
+                </p>
+            </div>
+            <div class="col-3 bloco-texto bloco-imagem">
+                <img src="./img/carros/fiat-toro.png">
+                <p><b>Fiat TORO</b></p>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting of the
+                    industry. Lorem
+                    Ipsum has been
+                    the and scrambled it.
+                    atype specimen
+                </p>
+            </div>
+        </div>
+        <div class="content" style="margin-left: 24%; float: left;">
+            <div class="col-3 bloco-texto bloco-imagem">
+                <img src="./img/carros/hyundai_hb20.png">
+                <p><b>Hyundai HB20</b></p>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting of the
+                    industry. Lorem
+                    Ipsum has been
+                    the and scrambled it.
+                    atype specimen
+                </p>
+            </div>
+            <div class="col-3 bloco-texto bloco-imagem">
+                <img src="./img/carros/peugeot-2008.png">
+                <p><b>Peugeot 2008</b></p>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting of the
+                    industry. Lorem
+                    Ipsum has been
+                    the and scrambled it.
+                    atype specimen
+                </p>
+            </div>
+            <div class="col-3 bloco-texto bloco-imagem">
+                <img src="./img/carros/renault_sandero.png">
+                <p><b>Reanult SANDEIRO</b></p>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting of the
+                    industry. Lorem
+                    Ipsum has been
+                    the and scrambled it.
+                    atype specimen
+                </p>
+            </div>
+        </div>
+    </div>
+    <footer>
+        <div class="footer-bottom">
+            <a href="https://www.instagram.com/juuniorcs/" target="_blank">
+                <p>@juuniorcs - 2023</p>
+            </a>
+        </div>
+    </footer>
     <script type="text/javascript" src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
     <script type="text/javascript" src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
     <script type="text/javascript" src="http://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
