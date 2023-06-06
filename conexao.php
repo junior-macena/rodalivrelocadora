a16eb1252f4ca9ba4e3f68feeff53a26f806060b
junior-macena <brjunior834@gmail.com>
2023-06-06 00:29:26 -0400

    update test

--- conexao.php
+++ conexao.php
@@ -1,17 +1,17 @@
 <?php
 //conexao com o Banco de dados
-	$host = '127.0.0.1';
-	$usuario = 'root';
-	$senha = '';
-	$database = 'locadoradb';
-	
-	date_default_timezone_set("America/Manaus");
-	
-	$mysqli = new mysqli($host, $usuario, $senha, $database);
+$host = 'locahost';
+$usuario = 'root';
+$senha = '';
+$database = 'locadoradb';
 
-	if($mysqli->error){
-		die("Falha ao conectar ao banco de dados: " . $mysqli->error);
-	}
+date_default_timezone_set("America/Manaus");
+
+$mysqli = new mysqli($host, $usuario, $senha, $database);
+
+if ($mysqli->error) {
+	die("Falha ao conectar ao banco de dados: " . $mysqli->error);
+}
 
 
 ?>
